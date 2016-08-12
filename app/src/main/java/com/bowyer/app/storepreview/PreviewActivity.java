af16115e554a70480e85fa3b88e357469d81940d
bowyer-app <bowyer.app@gmail.com>
2016-08-12 20:40:31 +0900

    add new line

--- app/src/main/java/com/bowyer/app/storepreview/PreviewActivity.java
+++ app/src/main/java/com/bowyer/app/storepreview/PreviewActivity.java
@@ -24,10 +24,12 @@ public class PreviewActivity extends AppCompatActivity implements ObservableScro
   private static String KEY_SHORT = "key_short";
   private static String KEY_DESCRIPTION = "key_description";
   private static String SHARE_FORMAT = "【簡単な説明文】\n%s\n【詳細な説明文】\n%s";
+
   @Bind(R.id.short_text) TextView shortText;
   @Bind(R.id.description) TextView description;
   @Bind(R.id.toolbar) Toolbar toolbar;
   @Bind(R.id.fab) FloatingActionButton fab;
+
   private String textShort;
   private String textDescription;
 
