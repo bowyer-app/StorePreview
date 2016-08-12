da53760ae014c0463567823b401e0db1ca4c119f
bowyer-app <bowyer.app@gmail.com>
2016-08-12 21:45:13 +0900

    add tag buttons

--- app/src/main/java/com/bowyer/app/storepreview/InputActivity.java
+++ app/src/main/java/com/bowyer/app/storepreview/InputActivity.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,15 +14,24 @@ import android.widget.EditText;
 import android.widget.Toast;
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import com.bowyer.app.fabtransitionlayout.FooterLayout;
 import com.bowyer.app.storepreview.preference.DataPreference;
 import com.crashlytics.android.Crashlytics;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollView;
+import com.github.ksoichiro.android.observablescrollview.ObservableScrollViewCallbacks;
+import com.github.ksoichiro.android.observablescrollview.ScrollState;
 import io.fabric.sdk.android.Fabric;
 
-public class InputActivity extends AppCompatActivity {
+public class InputActivity extends AppCompatActivity implements ObservableScrollViewCallbacks {
 
   private static final String KEY_DESCRIPTION = "key_description";
+
+  @Bind(R.id.scrollView) ObservableScrollView scrollView;
   @Bind(R.id.short_description) EditText editShortDescription;
   @Bind(R.id.description) EditText editDescription;
+  @Bind(R.id.fab) FloatingActionButton fab;
+  @Bind(R.id.fabtoolbar) FooterLayout footerLayout;
 
   private DataPreference mPrefs;
 
@@ -37,6 +48,8 @@ public class InputActivity extends AppCompatActivity {
     ButterKnife.bind(this);
     mPrefs = new DataPreference(this);
     initData();
+    scrollView.setScrollViewCallbacks(this);
+    footerLayout.setFab(fab);
   }
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -83,5 +96,109 @@ public class InputActivity extends AppCompatActivity {
       return;
     }
     PreviewActivity.startActivity(this, shortText, description);
+  }
+
+  @OnClick(R.id.fab) void onClickFab() {
+    footerLayout.expandFab();
+  }
+
+  @Override public void onScrollChanged(int scrollY, boolean firstScroll, boolean dragging) {
+
+  }
+
+  @Override public void onDownMotionEvent() {
+
+  }
+
+  @Override public void onUpOrCancelMotionEvent(ScrollState scrollState) {
+    if (scrollState == ScrollState.UP) {
+      footerLayout.contractFab();
+    } else if (scrollState == ScrollState.DOWN) {
+      footerLayout.contractFab();
+    }
+  }
+
+  private void setTag(String tag) {
+    int start = editDescription.getSelectionStart();
+    int end = editDescription.getSelectionEnd();
+    Editable editable = editDescription.getText();
+    editable.replace(Math.min(start, end), Math.max(start, end), tag);
+  }
+
+  @OnClick(R.id.font) void addFontTag() {
+    // TODO call color picker
+    setTag(getString(R.string.font));
+  }
+
+  @OnClick(R.id.font_end) void addFontEndTag() {
+    setTag(getString(R.string.font_end));
+  }
+
+  @OnClick(R.id.br) void addBrTag() {
+    setTag(getString(R.string.br));
+  }
+
+  @OnClick(R.id.b) void addBTag() {
+    setTag(getString(R.string.b));
+  }
+
+  @OnClick(R.id.b_end) void addBEndTag() {
+    setTag(getString(R.string.b_end));
+  }
+
+  @OnClick(R.id.i) void addITag() {
+    setTag(getString(R.string.i));
+  }
+
+  @OnClick(R.id.i_end) void addIEndTag() {
+    setTag(getString(R.string.i_end));
+  }
+
+  @OnClick(R.id.u) void addUTag() {
+    setTag(getString(R.string.u));
+  }
+
+  @OnClick(R.id.u_end) void addUEndTag() {
+    setTag(getString(R.string.u_end));
+  }
+
+  @OnClick(R.id.a) void addATag() {
+    setTag(getString(R.string.a));
+  }
+
+  @OnClick(R.id.a_end) void addAEndTag() {
+    setTag(getString(R.string.a_end));
+  }
+
+  @OnClick(R.id.h1) void addH1Tag() {
+    setTag(getString(R.string.h1));
+  }
+
+  @OnClick(R.id.h1_end) void addH1EndTag() {
+    setTag(getString(R.string.h1_end));
+  }
+
+  @OnClick(R.id.h2) void addH2Tag() {
+    setTag(getString(R.string.h2));
+  }
+
+  @OnClick(R.id.h2_end) void addH2EndTag() {
+    setTag(getString(R.string.h2_end));
+  }
+
+  @OnClick(R.id.h3) void addH3Tag() {
+    setTag(getString(R.string.h3));
+  }
+
+  @OnClick(R.id.h3_end) void addH3EndTag() {
+    setTag(getString(R.string.h3_end));
+  }
+
+  @OnClick(R.id.h4) void addH4Tag() {
+    setTag(getString(R.string.h4));
+  }
+
+  @OnClick(R.id.h4_end) void addH4EndTag() {
+    setTag(getString(R.string.h4_end));
   }
 }
